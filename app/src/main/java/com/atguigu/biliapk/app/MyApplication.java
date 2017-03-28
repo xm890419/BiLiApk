fbd6825e8fc88e53dc494dfaa148a1c0f8da6474
xm890419 <506083998@qq.com>
2017-03-28 17:52:03 +0800

    极光推送完成

--- app/src/main/java/com/atguigu/biliapk/app/MyApplication.java
+++ app/src/main/java/com/atguigu/biliapk/app/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by 熊猛 on 2017/3/20.
  */
@@ -20,6 +22,9 @@ public class MyApplication extends Application {
         context = this;
         threadid = android.os.Process.myPid();
         handler = new Handler();
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
 
     }
 
